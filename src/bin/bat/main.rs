a2e937c40dd2435218668c28da4dcb3fa33c581e
Patrick Pichler <git@patrickpichler.dev>
2021-10-06 20:32:47 +0200

    Add systemwide config file support
    There is now support for a systemwide config file. The location of the
    system wide config file is `$(BAT_SYSTEM_CONFIG_PREFIX)/bat/config`.
    `$(BAT_SYSTEM_CONFIG_PREFIX)` has to be provided at compile time as an
    environment variable. If the environment variable is not set, a default
    is used. This default is `C:\ProgramData` for windows and `/etc` for
    every other os.

--- src/bin/bat/main.rs
+++ src/bin/bat/main.rs
@@ -21,6 +21,9 @@ use crate::{
     config::{config_file, generate_config_file},
 };
 
+#[cfg(feature = "bugreport")]
+use crate::config::system_config_file;
+
 use assets::{assets_from_cache_or_binary, cache_dir, clear_assets, config_dir};
 use directories::PROJECT_DIRS;
 use globset::GlobMatcher;
@@ -252,6 +255,7 @@ fn invoke_bugreport(app: &App) {
             "NO_COLOR",
             "MANPAGER",
         ]))
+        .info(FileContent::new("System Config file", system_config_file()))
         .info(FileContent::new("Config file", config_file()))
         .info(CompileTimeInformation::default());
 
